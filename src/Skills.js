c0e97385e7466a666549efadf90b01450dc2380d
chaithanya2607 <rchaithanyaraot@gmail.com>
2022-11-14 18:46:09 +0530

    1

--- src/Skills.js
+++ src/Skills.js
@@ -3,42 +3,89 @@ import React from "react";
 export default function Skills() {
   return (
     <div>
-      <h1  style={{
-            color: "black",
-            fontSize: "80px",
-            fontFamily: "Papyrus",
-           
-          }}>Skills</h1>
+      <h1
+        style={{
+          color: "black",
+          fontSize: "50px",
+          fontFamily: "Papyrus",
+        }}
+      >
+        Skills
+      </h1>
       <div style={{ width: "600px", marginLeft: "200px" }}>
-    <div style={{ height: "30px", margin: "20px" }} className="progress">
-      <div
-        className="progress-bar progress-bar-striped progress-bar-animated"
-        role="progressbar"
-        style={{ width: "50%" }}
-      ><label>React js</label></div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            style={{ width: "50%" }}
+          >
+            <label>React js</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-info"
+            role="progressbar"
+            style={{ width: "50%" }}
+          >
+            <label>Node js</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            role="progressbar"
+            style={{ width: "75%" }}
+          >
+            <label>HTML</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+            role="progressbar"
+            style={{ width: "100%" }}
+          >
+            <label>CSS</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            style={{ width: "50%" }}
+          >
+            <label>Javascript</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-info"
+            role="progressbar"
+            style={{ width: "50%" }}
+          >
+            <label>Mongo db</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            role="progressbar"
+            style={{ width: "75%" }}
+          >
+            <label>Bootstrap</label>
+          </div>
+        </div>
+        <div style={{ height: "30px", margin: "20px" }} className="progress">
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+            role="progressbar"
+            style={{ width: "100%" }}
+          >
+            <label>My SQL</label>
+          </div>
+        </div>
+      </div>
     </div>
-    <div style={{ height: "30px", margin: "20px" }} className="progress">
-      <div
-        className="progress-bar progress-bar-striped progress-bar-animated bg-info"
-        role="progressbar"
-        style={{ width: "50%" }}
-      ><label>Node js</label></div>
-    </div>
-    <div style={{ height: "30px", margin: "20px" }} className="progress">
-      <div
-        className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
-        role="progressbar"
-        style={{ width: "75%" }}
-      ><label>HTML</label></div>
-    </div>
-    <div style={{ height: "30px", margin: "20px" }} className="progress">
-      <div
-        className="progress-bar progress-bar-striped progress-bar-animated bg-danger"
-        role="progressbar"
-        style={{ width: "100%" }}
-      ><label>CSS</label></div>
-    </div>
-  </div></div>
-    
   );
 }
